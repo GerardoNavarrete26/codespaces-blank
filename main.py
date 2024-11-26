6b28fa5988cafdd03bb07d3b9df468ad3177b68d
GerardoNavarrete26 <gerardo.navarrete@genstudents.org>
2024-11-26 15:35:34 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+contador = 0
+cantidad
+while contador < 5:  # Mientras contador sea menor que 5
+    print(contador)
+    contador += 1 
+nombre = input("Escribe tu nombre :")
+apellido = input("Escribe tu apellido :")
+nombrecompleto = nombre,apellido
+print(f"Hola, nombrecompleto. Como estas ?") 
+def suma(a, b):
+    return a + b
+print("Quieren sumar ?")
+a = input("Ingresa Numero 1 :")
+b = input("Ingresa Numero 2 :")
