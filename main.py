4e578e24b16e2a4865e258609c6e66124075a2cd
GerardoNavarrete26 <gerardo.navarrete@genstudents.org>
2024-11-26 16:28:09 +0000

    cambios 2

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 contador = 0
-cantidad
-while contador < 5:  # Mientras contador sea menor que 5
-    print(contador)
-    contador += 1 
+suma = 0
 nombre = input("Escribe tu nombre :")
 apellido = input("Escribe tu apellido :")
-nombrecompleto = nombre,apellido
-print(f"Hola, nombrecompleto. Como estas ?") 
-def suma(a, b):
-    return a + b
-print("Quieren sumar ?")
-a = input("Ingresa Numero 1 :")
-b = input("Ingresa Numero 2 :")
+nombrecompleto = nombre + " " + apellido
+print(f"Hola, {nombrecompleto}. Como estas ?") 
+
+def suma_numeros():
+    
+    cantidad = int(input("¿Cuántos números deseas sumar? "))
+    total = 0 
+    
+    for i in range(cantidad):
+        numero = float(input(f"Ingrese el número {i + 1}: "))
+        total += numero 
+    
+    print(f"La suma total es: {total}")
+
+suma_numeros()
